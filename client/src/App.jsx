ade82258bc3fd6b4ffb698e601ac16c09a7f04eb
Pushpam <pushpamkumari3122@gmail.com>
2025-11-13 19:41:36 +0530

    njj

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./components/Homepage";
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
+
